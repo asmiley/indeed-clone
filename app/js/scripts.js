7a1d4995a0c2c77d1703d84e6770427bb3c4a47c
asmiley <admin@asmiley.me>
2016-01-15 23:30:57 -0600

    Minor change to accordion function

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,13 +1,1 @@
-(function ($) {
-  $(document).ready(function () {
-
-    function toggleChevron(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find("i.indicator")
-        .toggleClass('glyphicon-triangle-right glyphicon-triangle-bottom');
-    }
-    $('#accordion').on('hidden.bs.collapse', toggleChevron);
-    $('#accordion').on('shown.bs.collapse', toggleChevron);
-  });
-})(jQuery);
+!function(n){n(document).ready(function(){function o(o){n(o.target).prev(".panel-heading").find("i.indicator").toggleClass("glyphicon-triangle-right glyphicon-triangle-bottom")}n("#accordion").on("hidden.bs.collapse",o),n("#accordion").on("shown.bs.collapse",o)})}(jQuery);
